355c7658b6eba6495ba45163573cee1d3a916e1f
David Novruzov <novruzovdavcho@gmail.com>
2025-06-09 23:15:45 +0400

    completing tour pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,6 @@
 const Tour = require('./../models/tourModel');
+const Review = require('./../models/reviewModel');
+const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res) => {
   //1) get tour data from the collection
@@ -10,8 +12,16 @@ exports.getOverview = catchAsync(async (req, res) => {
     tours: tours,
   });
 });
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The forest hiker',
+exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data for the requested tour with guides and reviews
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  //2)build template
+
+  //3)render template from step one
+  res.status(200).render('tour', {
+    tour,
+  });
+});
