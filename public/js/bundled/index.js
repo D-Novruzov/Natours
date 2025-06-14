424e631f4e14dc8abbe834233c69e8d7f428bee3
David Novruzov <novruzovdavcho@gmail.com>
2025-06-14 08:38:59 +0400

    implementing the user updateSettings

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -165,9 +165,12 @@
 var _runtime = require("regenerator-runtime/runtime");
 var _mapBox = require("./mapBox");
 var _login = require("./login");
+var _updateSettings = require("./updateSettings");
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('form-user-password');
 //values
 //delegation
 if (mapBox) {
@@ -181,8 +184,33 @@ if (loginForm) loginForm.addEventListener('submit', (e)=>{
     (0, _login.login)(email, password);
 });
 if (logoutBtn) logoutBtn.addEventListener('click', (0, _login.logout));
+if (userDataForm) userDataForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    (0, _updateSettings.updateSettings)({
+        name,
+        email
+    }, 'data');
+});
+if (userPasswordForm) userPasswordForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await (0, _updateSettings.updateSettings)({
+        passwordCurrent,
+        password,
+        passwordConfirm
+    }, 'password');
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+});
 
-},{"./login":"7yHem","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ","./mapBox":"k6XpQ"}],"7yHem":[function(require,module,exports,__globalThis) {
+},{"./login":"7yHem","core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ","./mapBox":"k6XpQ","./updateSettings":"l3cGY"}],"7yHem":[function(require,module,exports,__globalThis) {
 /* eslint-disable */ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "login", ()=>login);
@@ -6718,6 +6746,28 @@ const displayMap = (locations)=>{
     map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
 };
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["f2QDv"], "f2QDv", "parcelRequire11c7", {})
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"l3cGY":[function(require,module,exports,__globalThis) {
+//updateData funciton
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "updateSettings", ()=>updateSettings);
+var _axios = require("axios");
+var _axiosDefault = parcelHelpers.interopDefault(_axios);
+var _alerts = require("./alerts");
+const updateSettings = async (data, type)=>{
+    try {
+        const url = type === 'password' ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword' : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        const res = await (0, _axiosDefault.default)({
+            method: 'PATCH',
+            url: url,
+            data: data
+        });
+        if (res.data.status == 'success') (0, _alerts.showAlert)('success', ` ${type.toUpperCase()} updated successfully`);
+    } catch (err) {
+        (0, _alerts.showAlert)('err', err.response.data.nessage);
+    }
+};
+
+},{"axios":"jo6P5","./alerts":"6Mcnf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["f2QDv"], "f2QDv", "parcelRequire11c7", {})
 
 //# sourceMappingURL=index.js.map
--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,12 @@ import 'regenerator-runtime/runtime';
 import { displayMap } from './mapBox';
 import { login } from './login';
 import { logout } from './login';
+import { updateSettings } from './updateSettings';
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
-
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('form-user-password');
 //values
 //delegation
 if (mapBox) {
@@ -27,4 +29,32 @@ if (loginForm) {
 }
 if (logoutBtn) {
   logoutBtn.addEventListener('click', logout);
+}
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+
+    document.getElementById('password-confirm').value = '';
+  });
 }
